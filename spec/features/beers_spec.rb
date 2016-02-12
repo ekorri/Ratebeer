a84644fa5a3d921dc6743264d514bea2068a2285
ekorri <eevastiina.korri@helsinki.fi>
2016-02-12 14:01:04 +0200

    Vko4 ehkä

--- spec/features/beers_spec.rb
+++ spec/features/beers_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
+include Helpers
+
 describe "Beers"  do
    let!(:brewery) { FactoryGirl.create :brewery, name:"Koff" }
+   let!(:user) { FactoryGirl.create :user }
+
+   before :each do
+    sign_in(username:"Pekka", password:"Foobar1")
+   end
    
    it "saved if name is valid" do
    	visit new_beer_path
